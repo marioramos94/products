8683c32e0c9ffad7e98671c2f3bd1148088e316e
marioramos94 <32289432+marioramos94@users.noreply.github.com>
2019-03-25 15:41:40 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+//Components
+import Products from "./Products";
+
+class App extends Component {
+  render() {
+    return <Products />;
+  }
+}
+export default App;
